104142d753aae9cf31b8ba958fba1181affb2e3b
Christian Korndoerfer <christian@perspective.co>
2018-02-27 12:11:27 +0100

    updated Bootstrap and jQuerz

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -11,5 +11,5 @@
     </div>
 </footer>
 
-<script src="/assets/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="/assets/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
